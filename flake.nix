f4aa2dbc0f0884b7e97e07fd64055be8e06a51c2
Jun Matsushita <jun@iilab.org>
2022-04-16 18:31:48 +0200

    Refactor for multi platofrm

--- flake.nix
+++ flake.nix
@@ -9,9 +9,10 @@
 
     hls.url = "github:haskell/haskell-language-server";
     hls.inputs.nixpkgs.follows = "nixpkgs";
+
   };
 
-  outputs = { self, nixpkgs, flake-utils, hls }:
+  outputs = { self, nixpkgs, flake-utils, hls /*, rhine*/ }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -22,60 +23,59 @@
         # Change GHC version here. To get the appropriate value, run:
         #   nix-env -f "<nixpkgs>" -qaP -A haskell.compiler
         # hp = pkgs.haskellPackages;
-        hp = pkgs.haskell.packages.ghc922;
-
-        # https://github.com/NixOS/nixpkgs/issues/140774#issuecomment-976899227
-        m1MacHsBuildTools =
-          hp.override {
-            overrides = self: super:
-              let
-                workaround140774 = hpkg: with pkgs.haskell.lib;
-                  overrideCabal hpkg (drv: {
-                    enableSeparateBinOutput = false;
-                  });
-              in
-              {
-                ghcid = workaround140774 super.ghcid;
-                ormolu = workaround140774 super.ormolu;
-              };
-          };
-        # haskellPackages = pkgs.haskell.packages.ghc921.override  {
-        haskellPackages = pkgs.haskell.packages.ghc922.override  {
-          overrides = self: super: {
-            # In case you need them
-            network = pkgs.haskell.lib.dontCheck (self.callHackage "network" "3.1.2.5" {});
-          };
+        hp = pkgs.haskell.packages.ghc922.override {
+          overrides = self: super: (
+            if system == "aarch64-darwin"
+              then
+                let
+                  # https://github.com/NixOS/nixpkgs/issues/140774#issuecomment-976899227
+                  workaround140774 = hpkg: with pkgs.haskell.lib;
+                    overrideCabal hpkg (drv: {
+                      enableSeparateBinOutput = false;
+                    });
+                in
+                  {
+                    ghcid = workaround140774 super.ghcid;
+                  }
+              else {});
         };
+
         packageName ="gitpod-flake-ghc9";
+
+        project = returnShellEnv :
+          hp.developPackage {
+            inherit returnShellEnv;
+            name = packageName;
+            root = ./.;
+            overrides = self: super: {
+              # Use callCabal2nix to override Haskell dependencies here
+              # cf. https://tek.brick.do/K3VXJd8mEKO7
+              # Example:
+              # > NanoID = self.callCabal2nix "NanoID" inputs.NanoID { };
+              # Assumes that you have the 'NanoID' flake input defined.
+              network = pkgs.haskell.lib.dontCheck (self.callHackage "network" "3.1.2.5" {});
+              retry = pkgs.haskell.lib.dontCheck (self.callHackage "retry" "0.9.2.0" {});
+            };
+            modifier = drv:
+              pkgs.haskell.lib.addBuildTools drv (with hp; [
+                # Specify your build/dev dependencies here.
+                # cabal-fmt
+                cabal-install
+                ghcid
+                haskell-language-server
+                hls
+                hlint
+                fourmolu
+                pkgs.nixpkgs-fmt
+              ]);
+            };
       in {
 
-        packages.${packageName} =
-          haskellPackages.callCabal2nix packageName self rec {
-            # Link cabal extra-librarires to nix system packages
-            # zlib = pkgs.zlib;
-          };
+        # Used by `nix build` & `nix run` (prod exe)
+        defaultPackage = project false;
+        # Used by `nix develop` (dev shell)
+        devShell = project true;
 
-        defaultPackage = self.packages.${system}.${packageName};
-
-        devShell = haskellPackages.shellFor {
-          packages = p: [];
-
-          buildInputs =
-            (with (if system == "aarch64-darwin"
-            then m1MacHsBuildTools
-            else haskellPackages); [
-            pkgs.zlib
-            pkgs.zlib.dev
-
-            cabal-install
-            ghcid
-            haskell-language-server
-            hls
-            # hlint
-            ormolu
-          ]);
-          # withHoogle = true;
-        };
       }
     );
 }
