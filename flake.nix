121e7586531266b5379a840bbf64153cd4686e0f
Jun Matsushita <jun@iilab.org>
2022-04-16 15:24:57 +0200

    GHC 9.2.2 and M1 compat

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   description = "gitpod-flake-ghc9";
   nixConfig.bash-prompt = "\[develop\]$ ";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/21.11";
+    # nixpkgs.url = "github:NixOS/nixpkgs/21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/5181d5945eda382ff6a9ca3e072ed6ea9b547fee";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
 
     hls.url = "github:haskell/haskell-language-server";
     hls.inputs.nixpkgs.follows = "nixpkgs";
@@ -17,7 +19,28 @@
           config = { allowBroken = true; };
           overlays = [];
         };
-        haskellPackages = pkgs.haskell.packages.ghc921.override  {
+        # Change GHC version here. To get the appropriate value, run:
+        #   nix-env -f "<nixpkgs>" -qaP -A haskell.compiler
+        # hp = pkgs.haskellPackages;
+        hp = pkgs.haskell.packages.ghc922;
+
+        # https://github.com/NixOS/nixpkgs/issues/140774#issuecomment-976899227
+        m1MacHsBuildTools =
+          hp.override {
+            overrides = self: super:
+              let
+                workaround140774 = hpkg: with pkgs.haskell.lib;
+                  overrideCabal hpkg (drv: {
+                    enableSeparateBinOutput = false;
+                  });
+              in
+              {
+                ghcid = workaround140774 super.ghcid;
+                ormolu = workaround140774 super.ormolu;
+              };
+          };
+        # haskellPackages = pkgs.haskell.packages.ghc921.override  {
+        haskellPackages = pkgs.haskell.packages.ghc922.override  {
           overrides = self: super: {
             # In case you need them
             network = pkgs.haskell.lib.dontCheck (self.callHackage "network" "3.1.2.5" {});
@@ -37,17 +60,20 @@
         devShell = haskellPackages.shellFor {
           packages = p: [];
 
-          buildInputs = with haskellPackages; [
+          buildInputs =
+            (with (if system == "aarch64-darwin"
+            then m1MacHsBuildTools
+            else haskellPackages); [
             pkgs.zlib
             pkgs.zlib.dev
 
             cabal-install
             ghcid
-            # haskell-language-server
+            haskell-language-server
             hls
             # hlint
-            # ormolu
-          ];
+            ormolu
+          ]);
           # withHoogle = true;
         };
       }
